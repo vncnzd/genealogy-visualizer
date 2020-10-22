707fbe19ed1a8c1268afa4e361b4eb365bc821af
Vincenz Dziedzina <vincenz.dziedzina@gmail.com>
2020-10-22 15:16:25 +0200

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    plugins: [new MiniCssExtractPlugin({
+        filename: '../css/style.css'
+    })],
+    entry: './src/typescript/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    },
+                    {
+                        loader: 'ts-loader' 
+                    }
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader', // Translates CSS into CommonJS
+                    'sass-loader', // Compiles Sass to CSS
+                ],
+            },
+        ],
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist/js'),
+    },
+};
