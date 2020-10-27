84788e3a0c0f0072c6c9c80505bdc7672993d259
Vincenz Dziedzina <vincenz.dziedzina@gmail.com>
2020-10-27 14:28:19 +0100

    Add PersonController and PersonView classes

---
+++ src/typescript/controllers/personController.ts
@@ -0,0 +1,11 @@
+import { Person } from "../models/person";
+import { PersonView } from "../views/personView";
+
+export class PersonController {
+    private person: Person;
+    private personView: PersonView;
+
+    constructor(person: Person, personView: PersonView) {
+        
+    };
+}
