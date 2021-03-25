aca53464e810a84422425133af7b2f682d4bc0ec
Vincenz Dziedzina <vincenz.dziedzina@gmail.com>
2021-03-25 09:52:48 +0100

    Refactor SparqlQueryDispatcher class

--- src/typescript/sparqlQueryDispatcher.ts
+++ src/typescript/sparqlQueryDispatcher.ts
@@ -1,5 +1,3 @@
-import { LanguageIdentifier } from "./languageIdentifier";
-
 export class SPARQLQueryDispatcher {
 	private endpointUrl: string;
 	private numberOfConcurrentRequests: number;
@@ -17,16 +15,15 @@ export class SPARQLQueryDispatcher {
 		};
 	}
 
-	public async query(sparqlQuery): Promise<string> {
-		// makes sure, that only maxNumberOfConcurrentRequests requests are done at the same time
+	public async query(sparqlQuery: string): Promise<string> {
+		// This makes sure, that only maxNumberOfConcurrentRequests requests are made at the same time.
 		while (this.numberOfConcurrentRequests >= this.maxNumberOfConcurrentRequests) {
-			await new Promise(r => setTimeout(r, this.sleepTimeoutInMilliSeconds));
+			await this.sleep(this.sleepTimeoutInMilliSeconds);
 		}
 
 		const fullUrl: string = this.endpointUrl + '?query=' + encodeURIComponent(sparqlQuery);
-		
 		this.numberOfConcurrentRequests++;
-		console.log("Number of concurrent Requests: " + this.numberOfConcurrentRequests);
+		// console.log("Number of concurrent Requests: " + this.numberOfConcurrentRequests);
 		
 		return fetch(fullUrl, this.requestInit).then(response => {
 			return response.json(); 
@@ -40,9 +37,13 @@ export class SPARQLQueryDispatcher {
 	public setMaxNumberOfConcurrentRequests(maxNumberOfConcurrentRequests: number): void {
 		if (maxNumberOfConcurrentRequests < 1) {
 			this.maxNumberOfConcurrentRequests = 1;
-			console.info("There is an attempt to set the max number of concurrent requests below 1")
+			console.info("There is an attempt to set the max number of concurrent requests below 1.")
 		} else {
 			this.maxNumberOfConcurrentRequests = maxNumberOfConcurrentRequests;
 		}
+	}
+
+	private async sleep(durationInMilliseconds: number): Promise<unknown> {
+		return new Promise(resolve => setTimeout(resolve, durationInMilliseconds));
 	}
 }
