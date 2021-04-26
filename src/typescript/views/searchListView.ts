bce126d896e8d67fce119abf4ea47d16ad63f77f
Vincenz Dziedzina <vincenz.dziedzina@gmail.com>
2021-04-26 12:55:02 +0200

    Fix bug with wrong href of the language buttons when opening index as a file in a browser

--- src/typescript/views/searchListView.ts
+++ src/typescript/views/searchListView.ts
@@ -27,7 +27,7 @@ export class SearchListView extends View {
         parentElement.appendChild(languageButtonContainer);
 
         this.englishLinkElement = this.createHTMLElement("a");
-        this.englishLinkElement.setAttribute("href", window.location.origin + window.location.pathname);
+        this.englishLinkElement.setAttribute("href", window.location.origin + window.location.href);
         this.englishLinkElement.innerText = languageManager.getCurrentLanguageData()["englishButton"];
         languageButtonContainer.appendChild(this.englishLinkElement);
 
